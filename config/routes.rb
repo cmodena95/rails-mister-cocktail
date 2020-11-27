c531fb83369fd5a4e71053ca7af0b05817a62ba8
Camila Modena <itscamilla@hotmail.com>
2020-11-27 14:57:17 +0000

    Mister Cocktail Progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # get 'cocktails', to: 'cocktails#index'
+  # get 'cocktails/new', to: 'cocktails#new', as: :new
+  # get 'cocktails/:id', to: 'cocktails#show', as: :cocktail
+  # post 'cocktails', to: 'cocktails#create'
+  # get 'cocktails/:id/doses/new', to: 'doses#new'
+  # post 'cocktails/:id/doses', to: 'doses#create'
+
+  get '/', to: "cocktails#index"
+
+  resources :cocktails do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "brown sugar")
+Ingredient.create(name: "tequila")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "rum")
+Ingredient.create(name: "lime")
+Ingredient.create(name: "sherry")
+Ingredient.create(name: "cognac")
+Ingredient.create(name: "grapefruit")
+Ingredient.create(name: "red wine")
+Ingredient.create(name: "sugar syrup")
+Ingredient.create(name: "milk")
+Ingredient.create(name: "strawberry")
+Ingredient.create(name: "chocolate syrup")
+Ingredient.create(name: "mango")
+Ingredient.create(name: "ginger")
+Ingredient.create(name: "blueberry")
+Ingredient.create(name: "kiwi")
+Ingredient.create(name: "tomato juice")
+Ingredient.create(name: "white wine")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "cream")
+
+
